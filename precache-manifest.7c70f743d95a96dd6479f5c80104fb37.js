abd1a958d2a309d88e82a7c7ea59618f372ee7a2
Adam Susuł <a.w.susul@gmail.com>
2020-09-03 19:36:22 +0200

    Updates

---
+++ precache-manifest.7c70f743d95a96dd6479f5c80104fb37.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb7e7c125cb0b788f92ea8deb4734e9d",
+    "url": "/app-rr/index.html"
+  },
+  {
+    "revision": "b9200717782cabc63ad4",
+    "url": "/app-rr/static/css/main.8f2c44fb.chunk.css"
+  },
+  {
+    "revision": "e27e4fa56461627558fd",
+    "url": "/app-rr/static/js/2.09168819.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/app-rr/static/js/2.09168819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9200717782cabc63ad4",
+    "url": "/app-rr/static/js/main.2fcf0767.chunk.js"
+  },
+  {
+    "revision": "ee7617547d9f6458a2ac",
+    "url": "/app-rr/static/js/runtime-main.8a64c561.js"
+  },
+  {
+    "revision": "77f4e3eb14eabbcd12d93a52d9c587f7",
+    "url": "/app-rr/static/media/mountain_canion.77f4e3eb.jpg"
+  },
+  {
+    "revision": "85fe770c277e1465d2db4738f617ccaf",
+    "url": "/app-rr/static/media/mountain_car.85fe770c.jpg"
+  },
+  {
+    "revision": "e8338c269720d6d26d189d73afaffeae",
+    "url": "/app-rr/static/media/mountain_winter.e8338c26.jpg"
+  }
+]);
